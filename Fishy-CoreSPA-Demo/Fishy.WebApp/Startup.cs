1413c06570a5937a893143f255f942961e20bd66
JKobe <j.kobylko@hotmail.com>
2018-07-01 18:00:42 +0200

    add image browser + layout fixes

--- Fishy-CoreSPA-Demo/Fishy.WebApp/Startup.cs
+++ Fishy-CoreSPA-Demo/Fishy.WebApp/Startup.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Globalization;
+using System.IO;
 using System.Linq;
 using System.Net.Http;
 using System.Threading.Tasks;
@@ -11,6 +12,7 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.HttpOverrides;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.FileProviders;
 
 namespace Fishy.WebApp
 {
@@ -21,6 +23,7 @@ namespace Fishy.WebApp
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddMvc();
+            services.AddDirectoryBrowser();
 
             services.AddSingleton<IProductServices, ProductApiService>();
             services.AddSingleton(x => new HttpClient { BaseAddress = new Uri("http://localhost:5000") });
@@ -29,8 +32,23 @@ namespace Fishy.WebApp
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
         {
-            app.UseDeveloperExceptionPage();
+            app.UseStaticFiles();
 
+            app.UseStaticFiles(new StaticFileOptions
+            {
+                FileProvider = new PhysicalFileProvider(
+            Path.Combine(Directory.GetCurrentDirectory(), "wwwroot", "images")),
+                RequestPath = "/MyImages"
+            });
+
+            app.UseDirectoryBrowser(new DirectoryBrowserOptions
+            {
+                FileProvider = new PhysicalFileProvider(
+                Path.Combine(Directory.GetCurrentDirectory(), "wwwroot", "images")),
+                RequestPath = "/MyImages"
+            });
+
+            app.UseDeveloperExceptionPage();
             app.UseForwardedHeaders(new ForwardedHeadersOptions
             {
                 ForwardedHeaders = ForwardedHeaders.All
@@ -47,7 +65,7 @@ namespace Fishy.WebApp
                 await next();
             });
 
-            app.UseStaticFiles();
+           
 
             app.UseMvc(routes =>
             {
